c902ac348017d2252ca0a7d57bd5e460d84ddfa4
ramalingaa <87629304+ramalingaa@users.noreply.github.com>
2021-10-05 17:49:01 +0530

    feature: Adding new feature

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let colorPickerCell = document.querySelectorAll(".colorPicker");
 let headderElementA = document.querySelector("#headderElement");
 let emptyCells = document.querySelectorAll(".borderAlign");
 
+
 // Assign activecolor to emptyCell
 function getEmptyCell(cellId,activeColor){
     let clickedEmptyCell = document.getElementById(cellId);
@@ -10,11 +11,11 @@ function getEmptyCell(cellId,activeColor){
 
 
 for(let eachCell of colorPickerCell){
-    let cellId = Math.floor(Math.random()*12345678);
-    let randomHexaValue = "#" + cellId.toString(16);
+    let cellId = Math.floor(Math.random()*16777215);
+    let randomHexaValue =  cellId.toString(16);
 
     eachCell.id = cellId;
-    eachCell.style.backgroundColor = randomHexaValue;
+    eachCell.style.backgroundColor ="#" + randomHexaValue;
 
     eachCell.onclick = function(){
         clickedCellfun(cellId);
@@ -27,14 +28,14 @@ for(let eachCell of colorPickerCell){
 // getActiveColor function
 function clickedCellfun(cellId){
     let clickedCell =document.getElementById(cellId); 
-    let activeColor = window.getComputedStyle(clickedCell).getPropertyValue('background-color');
+    let activeColor = window.getComputedStyle(clickedCell).backgroundColor;
     console.log(activeColor);
+    console.log(cellId);
 
     // getEmptyCells
     for(let eachEmptyCell of emptyCells){
         let cellId = Math.floor(Math.random()*12345678);
         eachEmptyCell.id = cellId;
-    
         eachEmptyCell.onclick = function(){
             getEmptyCell(cellId,activeColor);
         }
