c4c73b24951d71ae1d1e901ebf314aff70980bb6
ramalingaa <87629304+ramalingaa@users.noreply.github.com>
2021-10-05 16:52:04 +0530

    docs:Main commit
    Adding code to the program

---
+++ app.js
@@ -0,0 +1,42 @@
+let colorPickerCell = document.querySelectorAll(".colorPicker");
+let headderElementA = document.querySelector("#headderElement");
+let emptyCells = document.querySelectorAll(".borderAlign");
+
+// Assign activecolor to emptyCell
+function getEmptyCell(cellId,activeColor){
+    let clickedEmptyCell = document.getElementById(cellId);
+    clickedEmptyCell.style.backgroundColor = activeColor;
+}
+
+
+for(let eachCell of colorPickerCell){
+    let cellId = Math.floor(Math.random()*12345678);
+    let randomHexaValue = "#" + cellId.toString(16);
+
+    eachCell.id = cellId;
+    eachCell.style.backgroundColor = randomHexaValue;
+
+    eachCell.onclick = function(){
+        clickedCellfun(cellId);
+        
+    }
+    
+   
+
+}
+// getActiveColor function
+function clickedCellfun(cellId){
+    let clickedCell =document.getElementById(cellId); 
+    let activeColor = window.getComputedStyle(clickedCell).getPropertyValue('background-color');
+    console.log(activeColor);
+
+    // getEmptyCells
+    for(let eachEmptyCell of emptyCells){
+        let cellId = Math.floor(Math.random()*12345678);
+        eachEmptyCell.id = cellId;
+    
+        eachEmptyCell.onclick = function(){
+            getEmptyCell(cellId,activeColor);
+        }
+    }
+}
